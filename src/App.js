3603caa5cb13b4ae373138accc064c487bae94bb
Sam Furr <samneillfurr@gmail.com>
2017-09-28 14:25:23 -0700

    Implement Geolocating and search
    Implement the geolocating via the geolocatior package and basic restaurant search in a set area.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MapView from './MapView';
+import SearchBox from './SearchBox'
 
 class App extends Component {
 
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="wrapper">
         <MapView />
+        <SearchBox />
       </div>
     );
   }
---
+++ src/SearchBox.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import geolocator from 'geolocator';
+
+class SearchBox extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      searchValue: '',
+      results: [{name: "sammy"}]
+    }
+    geolocator.config({
+        language: "en",
+        google: {
+            version: "3",
+            key: "AIzaSyBrLxlfWBEnisKWdAfPaxd7WlJMpvpl4R8"
+        }
+    });
+  }
+
+  handleChange(event) {
+    this.setState({searchValue: event.target.value});
+  }
+
+  handleSubmit(event){
+    let lat = null;
+    let lon = null;
+    event.preventDefault();
+    var service = new window.google.maps.places.PlacesService(document.createElement('div'));
+    geolocator.geocode(this.state.searchValue, function (err, location) {
+      if(err){
+        alert(err)
+      }else{
+        lat = location.coords.latitude;
+        lon = location.coords.longitude;
+        var request = {
+          location: new window.google.maps.LatLng(lat,lon),
+          radius: '500',
+          type: ['restaurant']
+        };
+
+        service.nearbySearch(request, function(results, status){
+          this.setState({results: results});
+        }.bind(this));
+      }
+    }.bind(this));
+
+  }
+
+  render(){
+    return(
+      <div className="searchbox">
+      <form onSubmit={this.handleSubmit}>
+        <input
+        type="text"
+        placeholder="Search..."
+        value={this.state.searchValue}
+        onChange={this.handleChange}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+
+      <div className="resultsbox">
+        {this.state.results.map(function(x){
+          return <Result name={x.name} />
+        })}
+      </div>
+      </div>
+    )
+  }
+}
+
+class Result extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const name = this.props.name;
+    return (
+      <div className="resultbox">
+        <strong>{name}</strong>
+      </div>
+    );
+  }
+}
+
+export default SearchBox;
