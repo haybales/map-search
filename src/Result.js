0dcf4dfa910aa645f3942cd330710f8cc6ebef70
Sam Furr <samneillfurr@gmail.com>
2017-10-04 19:21:30 -0700

    clean up Result, convert to function

--- src/Result.js
+++ src/Result.js
@@ -1,32 +1,22 @@
 import React, { Component } from 'react';
 
-class Result extends Component {
-  constructor(props) {
-    super(props);
-  }
+function Result(props){
 
-  setRender(){
-    const name = this.props.name;
-    const desc = this.props.desc;
-    //replace the underscores so it looks better.
-    const type = this.props.type.replace(/_/g, " ");
-    let classes="resultbox"
-    //decide if the selection is focussed
-    if(this.props.focus===this.props.xid){
-      classes+=" focussed"
-    }
-    return (
-      <div className={classes} onClick={function(){this.props.setFocus(this.props.xid)}.bind(this)}>
-        <strong>{name}</strong> - <span>{type}</span><p>{desc}</p>
-      </div>
-    );
-  }
+  //replace the underscores so it looks better.
+  const type = props.type.replace(/_/g, " ");
+  const name = props.name;
+  const desc = props.desc;
 
-  render() {
-    return (
-      this.setRender()
-    );
+  let classes="resultbox"
+  //decide if the selection is focussed
+  if(props.focus===props.xid){
+    classes+=" focussed"
   }
+  return (
+    <div className={classes} onClick={function(){props.setFocus(props.xid)}.bind(this)}>
+      <strong>{name}</strong> - <span>{type}</span><p>{desc}</p>
+    </div>
+  );
 }
 
 export default Result
